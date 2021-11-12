e525951377c53f8d1a761dcfe7c12135d98abff7
Daisy Guerrero <dguerrero1@bsd220.org>
2021-11-11 20:26:54 -0600

    Created Initial View

--- Final Project/ContentView.swift
+++ Final Project/ContentView.swift
@@ -9,8 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Text("Brick, Brick, Break!")
+                    .font(.system(size: 40))
+                    .font(.title)
+                    .fontWeight(.heavy)
+                    .padding()
+                LazyVGrid(columns: Array(repeating: GridItem(.fixed(120), spacing: 8), count: 3), spacing: 8, content: {
+                    ForEach(0..<9) { index in
+                        Color.white
+                            .frame(width: 100, height: 50)
+                    }
+                })
+                .padding()
+                NavigationLink("PLAY", destination: LevelSelect())
+                    .padding()
+                Spacer()
+                .preferredColorScheme(.dark)
+            }
+        }
     }
 }
 
---
+++ Final Project/LevelSelect.swift
@@ -0,0 +1,20 @@
+//
+//  LevelSelect.swift
+//  Final Project
+//
+//  Created by Student on 11/10/21.
+//
+
+import SwiftUI
+
+struct LevelSelect: View {
+    var body: some View {
+        Text("Select Level")
+        }
+}
+
+struct LevelSelect_Previews: PreviewProvider {
+    static var previews: some View {
+        LevelSelect()
+    }
+}
