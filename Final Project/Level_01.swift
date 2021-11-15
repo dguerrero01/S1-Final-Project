7a91d8624428ae5158c378c69518c086346b3f36
Daisy Guerrero <dguerrero1@bsd220.org>
2021-11-15 10:57:55 -0600

    Level 1 v1

--- Final Project/Level_01.swift
+++ Final Project/Level_01.swift
@@ -9,7 +9,31 @@ import SwiftUI
 
 struct Level_01: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                Text("Level 1")
+                    .font(.system(size: 40))
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .padding()
+                LazyVGrid(columns: Array(repeating: GridItem(.fixed(120), spacing: -30), count: 4), spacing: 8, content: {
+                    ForEach(0..<16) { index in
+                        Color.white
+                            .frame(width: 80, height: 40)
+                    }
+                })
+                .padding()
+                HStack {
+                    Text("Score: ")
+                        .padding()
+                    Text("Time Left: ")
+                        .padding()
+                }
+                Spacer()
+                    .preferredColorScheme(.dark)
+                NavigationLink("Next Level", destination: Level_02())
+            }
+        }
     }
 }
 
